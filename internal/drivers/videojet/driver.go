c7e241c48c05544a667cc50b29eea143a701bc6b
zmich <zmichok@gmail.com>
2026-05-10 12:45:15 +0300

    +

--- internal/drivers/videojet/driver.go
+++ internal/drivers/videojet/driver.go
@@ -184,7 +184,7 @@ func (d *Driver) GetStatus() (string, error) {
 
 // GetRemainingRibbon использует команду GCL (Consumable Levels) [cite: 1086]
 func (d *Driver) GetRemainingRibbon() (string, error) {
-	raw, err := d.sendRaw("GST")
+	raw, err := d.sendRaw("GCL")
 	if err != nil {
 		return "", err
 	}
